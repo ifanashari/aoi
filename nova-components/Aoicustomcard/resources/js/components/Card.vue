9869805dea83ee2443e85903a2b211af5612410a
ifanashari <ifanashari1234@gmail.com>
2019-08-01 17:27:44 +0700

    add | custom-card
    vue-chartjs

---
+++ nova-components/Aoicustomcard/resources/js/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="c-aoi-card">
+        <f-chart class="f-chart" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'card',
+
+        // The following props are only available on resource detail cards...
+        // 'resource',
+        // 'resourceId',
+        // 'resourceName',
+    ],
+
+    mounted() {
+        //
+    },
+}
+</script>
